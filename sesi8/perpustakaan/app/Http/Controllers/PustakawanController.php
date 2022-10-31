b357c2193dd7596a484c412110e3a7ae83ead9ec
annisam23 <annisamrahma23@gmail.com>
2022-10-31 10:35:57 +0700

    pertemuan 8

---
+++ sesi8/perpustakaan/app/Http/Controllers/PustakawanController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pustakawan;
+use Illuminate\Http\Request;
+
+class PustakawanController extends Controller
+{
+    function index()
+    {
+        $pustakawan = Pustakawan::all();
+
+        return response()->json([
+            'message' => 'Get all resources',
+            'status code' => true,
+            'data' => $pustakawan,
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $input = [
+            'name' => $request->name,
+            'address' => $request->address,
+            'age' => $request->age,
+            'nik' => $request->rating,
+        ];
+
+        //menggunkaan model pustakawna uuntuk insertd data
+        $book = Pustakawan::create($input);
+
+        $data = [
+            'message' => "Book is created succesfully",
+            'data' => $book
+        ];
+
+        //mengembalikan data json dan kode 201
+        return response()->json($data, 201);
+    }
+
+    public function show($id)
+    {
+        $pustakawanDetails = Pustakawan::find($id);
+
+        // jika id tidak ditemukan
+        if (!$pustakawanDetails) {
+            return response()->json([
+                'message' => 'Resource not found',
+                'status' => 404,
+            ], 404);
+        }
+
+        //return book return
+        return response()->json([
+            'message' => 'get detail resource',
+            'status' => 200,
+            'data' => $pustakawanDetails
+        ], 200);
+    }
+}
---
+++ sesi8/perpustakaan/summarize.md
@@ -0,0 +1,5 @@
+#Jarvis pertemuan 8
+Membahas tentang laravel lanjutan
+yaitu add resource dan show detail
+add resource-> menggunakan method store untuk menambah data ke database
+show detail -> menggunakan method show untuk melihat secara detail dengan id tertentu
